b0b4f473b3ed3a52302b7fc6154490aae6dd4ffa
Alina <alina.rebzon@mail.ru>
2022-07-21 18:07:56 +0800

    1111

--- client/src/App.vue
+++ client/src/App.vue
@@ -11,6 +11,7 @@ import { RouterLink, RouterView } from 'vue-router'
         <RouterLink to="/">Домашняя</RouterLink>
          <RouterLink to="/teachers">Преподаватели</RouterLink>
          <RouterLink to="/students">Обучающиеся</RouterLink>
+         <RouterLink to="/schools">Образовательные организации</RouterLink>
          <RouterLink to="/reports">Отчеты</RouterLink>
         <RouterLink to="/about">О приложении</RouterLink>
         <RouterLink to="/about">Выход</RouterLink>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import students from '../components/students.vue'
+import students from '../views/students.vue'
+import teachers from '../views/teachers.vue'
+import schools from '../views/schools.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +16,16 @@ const router = createRouter({
       path: '/students',
       name: 'students',
       component: students
+    },
+    {
+      path: '/teachers',
+      name: 'teachers',
+      component: teachers
+    },
+    {
+      path: '/schools',
+      name: 'schools',
+      component: schools
     },
   ]
 })
